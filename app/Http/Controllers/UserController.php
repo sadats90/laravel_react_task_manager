879565dc5f2ffb099cbe85a37ebe4f3ecc4b2c5b
sadats90 <sadatshahriar.ss@gmail.com>
2024-10-06 23:59:05 +0600

    usercontroller methods added

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,10 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\UserCrudResource;
 use App\Models\User;
 use App\Http\Requests\StoreUserRequest;
 use App\Http\Requests\UpdateUserRequest;
-use App\Http\Resources\UserCrudResource;
 
 class UserController extends Controller
 {
@@ -14,7 +14,7 @@ class UserController extends Controller
      */
     public function index()
     {
-         $query = User::query();
+        $query = User::query();
 
         $sortField = request("sort_field", 'created_at');
         $sortDirection = request("sort_direction", "desc");
@@ -72,7 +72,9 @@ class UserController extends Controller
      */
     public function edit(User $user)
     {
-        //
+        return inertia('User/Edit', [
+            'user' => new UserCrudResource($user),
+        ]);
     }
 
     /**
@@ -80,7 +82,17 @@ class UserController extends Controller
      */
     public function update(UpdateUserRequest $request, User $user)
     {
-        //
+        $data = $request->validated();
+        $password = $data['password'] ?? null;
+        if ($password) {
+            $data['password'] = bcrypt($password);
+        } else {
+            unset($data['password']);
+        }
+        $user->update($data);
+
+        return to_route('user.index')
+            ->with('success', "User \"$user->name\" was updated");
     }
 
     /**
@@ -88,6 +100,9 @@ class UserController extends Controller
      */
     public function destroy(User $user)
     {
-        //
+        $name = $user->name;
+        $user->delete();
+        return to_route('user.index')
+            ->with('success', "User \"$name\" was deleted");
     }
 }
--- resources/js/Pages/User/Edit.jsx
+++ resources/js/Pages/User/Edit.jsx
@@ -8,11 +8,10 @@ import { Head, Link, useForm } from "@inertiajs/react";
 
 export default function Create({ auth, user }) {
   const { data, setData, post, errors, reset } = useForm({
-    image: "",
     name: user.name || "",
-    status: user.status || "",
-    description: user.description || "",
-    due_date: user.due_date || "",
+    email: user.email || "",
+    password: "",
+    password_confirmation: "",
     _method: "PUT",
   });
 
@@ -42,25 +41,6 @@ export default function Create({ auth, user }) {
               onSubmit={onSubmit}
               className="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg"
             >
-              {user.image_path && (
-                <div className="mb-4">
-                  <img src={user.image_path} className="w-64" />
-                </div>
-              )}
-              <div>
-                <InputLabel
-                  htmlFor="user_image_path"
-                  value="User Image"
-                />
-                <TextInput
-                  id="user_image_path"
-                  type="file"
-                  name="image"
-                  className="mt-1 block w-full"
-                  onChange={(e) => setData("image", e.target.files[0])}
-                />
-                <InputError message={errors.image} className="mt-2" />
-              </div>
               <div className="mt-4">
                 <InputLabel htmlFor="user_name" value="User Name" />
 
@@ -77,54 +57,56 @@ export default function Create({ auth, user }) {
                 <InputError message={errors.name} className="mt-2" />
               </div>
               <div className="mt-4">
-                <InputLabel
-                  htmlFor="user_description"
-                  value="User Description"
-                />
+                <InputLabel htmlFor="user_email" value="User Email" />
 
-                <TextAreaInput
-                  id="user_description"
-                  name="description"
-                  value={data.description}
+                <TextInput
+                  id="user_email"
+                  type="text"
+                  name="email"
+                  value={data.email}
                   className="mt-1 block w-full"
-                  onChange={(e) => setData("description", e.target.value)}
+                  onChange={(e) => setData("email", e.target.value)}
                 />
 
-                <InputError message={errors.description} className="mt-2" />
+                <InputError message={errors.email} className="mt-2" />
               </div>
+
+              <div className="mt-4">
+                <InputLabel htmlFor="user_password" value="Password" />
+
+                <TextInput
+                  id="user_password"
+                  type="password"
+                  name="password"
+                  value={data.password}
+                  className="mt-1 block w-full"
+                  onChange={(e) => setData("password", e.target.value)}
+                />
+
+                <InputError message={errors.password} className="mt-2" />
+              </div>
+
               <div className="mt-4">
                 <InputLabel
-                  htmlFor="user_due_date"
-                  value="User Deadline"
+                  htmlFor="user_password_confirmation"
+                  value="Confirm Password"
                 />
 
                 <TextInput
-                  id="user_due_date"
-                  type="date"
-                  name="due_date"
-                  value={data.due_date}
+                  id="user_password_confirmation"
+                  type="password"
+                  name="password_confirmation"
+                  value={data.password_confirmation}
                   className="mt-1 block w-full"
-                  onChange={(e) => setData("due_date", e.target.value)}
+                  onChange={(e) =>
+                    setData("password_confirmation", e.target.value)
+                  }
                 />
 
-                <InputError message={errors.due_date} className="mt-2" />
-              </div>
-              <div className="mt-4">
-                <InputLabel htmlFor="user_status" value="User Status" />
-
-                <SelectInput
-                  name="status"
-                  id="user_status"
-                  className="mt-1 block w-full"
-                  onChange={(e) => setData("status", e.target.value)}
-                >
-                  <option value="">Select Status</option>
-                  <option value="pending">Pending</option>
-                  <option value="in_progress">In Progress</option>
-                  <option value="completed">Completed</option>
-                </SelectInput>
-
-                <InputError message={errors.user_status} className="mt-2" />
+                <InputError
+                  message={errors.password_confirmation}
+                  className="mt-2"
+                />
               </div>
               <div className="mt-4 text-right">
                 <Link
